acef4a5d9ac282ade798aadbd70506e2098c34e4
OlyaNovik <olyaanovikk@gmail.com>
2022-12-02 18:53:39 +0200

    CV

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/HeaderLoyaut/Layout";
+import Home from "./pages/HomeCv/Home";
+import Contact from "./pages/Contact";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello1</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
